bb3a82c5ec7a3e096df8912c080e4c45312d8dc1
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-04-26 13:52:41 +0530

    Asset Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
-import 'dart:ui'; //android run permission
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold( //Scaffold like a structure other word it is a skeleton
-        backgroundColor: Colors.blueAccent, //background color
-        appBar: AppBar( //app nevi.. bar
-
-          centerTitle: true, //text center
-          title:  Text("Varanasi Software Junction"),
-          backgroundColor: Colors.blueGrey, //appbar background color
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Local Image"),
+          backgroundColor: Colors.blueGrey,
         ),
-
-        body:
-
-        Center(child: Image(
-          image: NetworkImage( //sorce code
-              "https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png"),
-        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/vsjpl.png"),
+          ),
         ),
       ),
     ),
