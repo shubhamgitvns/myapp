6d2baa389e0f5162ee6eefd9f3f9286f30b0634f
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-04-26 12:58:19 +0530

    add internet img

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'dart:ui'; //android run permission
+
 import 'package:flutter/material.dart';
-//Centerd Text
+
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Varanasi Software Junction"),
+      home: Scaffold( //Scaffold like a structure other word it is a skeleton
+        backgroundColor: Colors.blueAccent, //background color
+        appBar: AppBar( //app nevi.. bar
+
+          centerTitle: true, //text center
+          title:  Text("Varanasi Software Junction"),
+          backgroundColor: Colors.blueGrey, //appbar background color
+        ),
+
+        body:
+
+        Center(child: Image(
+          image: NetworkImage( //sorce code
+              "https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png"),
+        ),
+        ),
       ),
     ),
   );
-
 }
