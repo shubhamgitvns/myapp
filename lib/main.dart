f0c51d88386a9772434a3f35b64b744c85e10042
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-03 16:28:20 +0530

    Add Icon as string value

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,196 @@
+
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:myapp/question.dart';
 
 void main() {
-  runApp(VSJQuizApp());
+runApp(VSJQuizApp());
 }
 
-class VSJQuizApp extends StatelessWidget {   //StatelessWidget body
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Scaffold(
-            appBar: AppBar(          //App bar body
-              backgroundColor: Colors.teal,
-              title: const Card(
-                  child: Text(
-                    "Quiz App",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      backgroundColor: Colors.teal,
-                    ),
-                  )),
-              centerTitle: true,
-            ),
-            backgroundColor: Colors.grey.shade900,
-            body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: VSJQuiz(),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+class VSJQuizApp extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+debugShowCheckedModeBanner: false,
+home: SafeArea(
+child: Padding(
+padding: const EdgeInsets.all(8.0),
+child: Scaffold(
+appBar: AppBar(
+backgroundColor: Colors.teal,
+title: Card(
+child: Text(
+"Quiz App",
+style: TextStyle(
+fontSize: 40,
+fontWeight: FontWeight.bold,
+backgroundColor: Colors.teal,
+),
+)),
+centerTitle: true,
+),
+backgroundColor: Colors.grey.shade900,
+body: SafeArea(
+child: Padding(
+padding: EdgeInsets.symmetric(horizontal: 10.0),
+child: VSJQuiz(),
+),
+),
+),
+),
+),
+);
+}
 }
 
 class VSJQuiz extends StatefulWidget {
-  @override
-  _VSJQuizState createState() => _VSJQuizState();
+@override
+_VSJQuizState createState() => _VSJQuizState();
 }
 
 class _VSJQuizState extends State<VSJQuiz> {
-  int questionno = -1;
-  int correctAnswers = 0;
-  String buttonlabel = "Submit";
-  //String currentquestiontext = "Press any button to start the quiz";
-  List<Widget> scores = [];
-
-  List<Question> questions = [ //Question list
-    Question("What is the capital of India", "Lucknow", "Delhi", "Hukulgunj",
-        "Pandaypur",2),
-    Question("Whear is situated The Sundarpur", "Hukulgunj", "Bhojubir",
-        "Lunka", "Jaytpura", 3),
-    Question("What is the full form of Cr", "Criminal recerch",
-        "Class Representative", "Champak Roy", "Shubham",3),
-  ];
-  String questiontext = "Start the Quiz";
-  Question currentquestion = Question("", "", "", "", "",0);
+String currentquestiontext = "Press any button to start the quiz";
+int questionno = -1;
+int correctanswers = 0;
+bool isTestOver = false;
+List<Question> questions = [
+new Question("C is a programming language? T/F", "true"),
+new Question("C++ is not an object oriented language.. T/F", "false"),
+new Question("Python has dictionary.. T/F", "true"),
+new Question("Hukulganj is the capital of Japan T/F", "false"),
+new Question("Shubham Is Cr In The Class","false")
+];
+Question? currentquestion;
+List<Widget> scores = [];
 
 
-  void addResult(bool b) {
-    bool iscorrect = b == currentquestion.correctAnswer;
-    //scores.clear();
-    if (iscorrect) {
-      correctAnswers++;
-      scores.add( Icon(Icons.check, color: Colors.green));
-    } else {
-      scores.add(Icon(Icons.close, color: Colors.red));
-    }
-  }
 
+void setQuestion(String s) {
+//isTestOver=false;
+//questionno=-1;
+//scores.clear();
 
-  void nextquestion() { //Class method
-    questionno++;
-    print(questionno);
+if (isTestOver) return;
 
-    if (questionno >= questions.length) {
-      questiontext = "Test over .correctCorrect answers = $correctAnswers"; // Here Question-text value change
-      // Show the current question option null
-      currentquestion.opta="";
-      currentquestion.optb="";
-      currentquestion.optc="";
-      currentquestion.optd="";
-      buttonlabel = "Restart"; // Here button-level value change
-      questionno=-1; // Restart program
-      return;
-    }
-    currentquestion = questions[questionno];
-    questiontext = currentquestion.question;
-  }
+if (questionno == -1) {
+questionno++;
+currentquestion = questions[questionno];
+currentquestiontext = currentquestion!.question;
+return;
+}
 
+if (questionno >= questions.length - 1) {
+addResult(s);
+currentquestiontext = "Questions Over. Correct answers = $correctanswers";
+isTestOver = true;
+return;
+}
 
-  int _selectedOption = 0;
-  void _handleOptionChange(int ? value) {
-    setState(() {
-      _selectedOption =value! ;
-    });
-  }
+addResult(s);
+questionno++;
+if (questionno <= questions.length - 1) {
+currentquestion = questions[questionno];
+currentquestiontext = currentquestion!.question;
+}
+}
 
+ void addResult(String s) {
+bool iscorrect = s == currentquestion!.correctAnswer;
+//scores.clear();
+if (iscorrect) {
+correctanswers++;
+scores.add(Icon(Icons.check, color: Colors.green));
+} else {
+scores.add(Icon(Icons.close, color: Colors.red));
+}
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 3,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                questiontext,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 5,
-          child: Center(
-            child: GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 3.0,
-              padding: const EdgeInsets.all(10.0),
-              children: <Widget>[
-                RadioListTile(
-                  title:  Text(currentquestion.opta,
-                      style: const TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.white,
-                      )),
-                  value: 1,
-                  groupValue: _selectedOption,
-                  onChanged: _handleOptionChange,
-                ),
-                RadioListTile(
-                  title:  Text(currentquestion.optb,
-                      style: const TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.white,
-                      )),
-                  value: 2,
-                  groupValue: _selectedOption,
-                  onChanged: _handleOptionChange,
-                ),
-                RadioListTile(
-                  title:  Text(currentquestion.optc,
-                      style: const TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.white,
-                      )),
-                  value: 3,
-                  groupValue: _selectedOption,
-                  onChanged: _handleOptionChange,
-                ),
-                RadioListTile(
-                  title:  Text(currentquestion.optd,
-                      style: const TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.white,
-                      )),
-                  value: 4,
-                  groupValue: _selectedOption,
-                  onChanged: _handleOptionChange,
-                ),
-              ],
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-                  minimumSize: const Size.fromHeight(50),
-                ),
-                child:  Text(
-                  buttonlabel,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () {       //onPressed
-                  nextquestion();
-                  _selectedOption = 0;
-                  setState(() {});
-                  print(questionno);
-                }),
-          ),
-        ),
-        Row(
-          children: scores,
-        ),
-      ],
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return Column(
+mainAxisAlignment: MainAxisAlignment.spaceBetween,
+crossAxisAlignment: CrossAxisAlignment.stretch,
+children: <Widget>[
+Expanded(
+flex: 5,
+child: Padding(
+padding: EdgeInsets.all(10.0),
+child: Center(
+child: Text(
+currentquestiontext,
+textAlign: TextAlign.center,
+style: TextStyle(
+fontSize: 25.0,
+color: Colors.white,
+),
+),
+),
+),
+),
+Expanded(
+child: Padding(
+padding: EdgeInsets.all(15.0),
+child: ElevatedButton(
+style: ElevatedButton.styleFrom(
+primary: Colors.green,
+minimumSize: const Size.fromHeight(50),
+),
+child: Text(
+'True',
+style: TextStyle(
+color: Colors.white,
+fontSize: 20.0,
+),
+),
+onPressed: () {
+print("Submitted True");
+setState(() {
+
+setQuestion("true");
+});
+}),
+),
+),
+Expanded(
+child: Padding(
+padding: EdgeInsets.all(15.0),
+child: ElevatedButton(
+style: ElevatedButton.styleFrom(
+primary: Colors.red,
+minimumSize: const Size.fromHeight(50),
+),
+child: Text(
+'False',
+style: TextStyle(
+fontSize: 20.0,
+color: Colors.white,
+),
+),
+onPressed: () {
+print("Submitted False");
+setState(() {
+ 
+setQuestion('false');
+});
+},
+),
+),
+),
+Row(
+children: scores,
+),
+],
+);
+}
+}
+
+class Question {
+String question = "";
+String correctAnswer = "";
+
+Question(String question, String correctAnswer) {
+this.question = question;
+this.correctAnswer = correctAnswer;
+}
 }
