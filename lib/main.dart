cd8ac3b0f0dcd5a4cd473612688a8adfd5796378
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-08 14:16:05 +0530

    Basic api calling fun() and show in display

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,67 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("App"),
+          title: Text("My App"),
+          centerTitle: true,
         ),
         body: const MyStful(),
       ),
     ));
   }
 }
-
 class MyStful extends StatefulWidget {
   const MyStful({Key? key}) : super(key: key);
-
+  
   @override
   State<MyStful> createState() => _MyStfulState();
 }
 
 class _MyStfulState extends State<MyStful> {
   String link =
-      "/champaksworldcreate/320e5af5ea9dbd31597d220637885587/raw/99f8f7a4df34ae477dcceb62598aa0bdde9ef685/tfquestions.json";
+      "/shubhamgitvns/89d337387aaf2d1f2f134a51fd327078/raw/01ba387cd5c7fef05815a30b72ad3a7b955f0d9e/data.json";
+
   int counter = 0;
-  String message = "Clicked ";
+  String message = "Clicked";
+  String one="Name";
+  String two="Roll no.";
+  String three="Stream";
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Row(
+        //   children: [
+        //     Expanded(
+        //         child:Center(child: Text(""),))
+        //   ],
+        // )
         ElevatedButton(
-          child: Text("Clicked $counter times."),
-          onPressed: () async {
-            counter++;
-            dynamic data = await Utilities.f(link);
-            print(data);
-            setState(() {});
-          },
-        ),
+            child: Text("Clicked $counter times."),
+        onPressed: () async {
+              counter++;
+              dynamic data = await Utilities.f(link);
+              print(data);
+              print(data["name"]);
+              one=data["name"];
+              print(data["rollno"]);
+              two=data["rollno"];
+              three=data["stream"];
+              print(data["stream"]);
+              setState(()  {
+
+              });
+        }),
+
+        Row(
+          children:  [
+            Expanded(child: Text(one)),
+            Expanded(child: Text(two)),
+            Expanded(child: Text(three)),
+          ],
+        )
       ],
+
     );
   }
 }
+
