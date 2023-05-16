f683c1ca25dbe80ceb4effa22232b74a10864a81
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-16 16:56:41 +0530

    Create Appbar using stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(
+void main()=>runApp( const MyApp());
 
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(" I am App"),
-        centerTitle: true,
-      )
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent, //background color
+       appBar: AppBar(
+         backgroundColor: Colors.blueGrey,
+         title: Text("Appbar"),
+         centerTitle: true,
+       ),
+        body: Column(
+          children: [
+            Expanded(child: Center(child: Text("I am body"),))
+          ],
+        ),
+
       ),
-    ),
-  );
+    );
+  }
+}
