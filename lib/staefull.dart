e0dd28b703f32aee76c5fef77523537bc2e451fa
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-24 20:14:16 +0530

    Print nested object{ {..} } data from Json file

--- lib/staefull.dart
+++ lib/staefull.dart
@@ -15,11 +15,11 @@ class _ComplexDataState extends State<ComplexData> {
   Future<void> getUserApi () async{
     final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
     if(response.statusCode==200){
+      print(response.statusCode);
       data=jsonDecode(response.body.toString());
       print(response.body);
     }
     else{
-
     }
 
 
@@ -38,9 +38,27 @@ class _ComplexDataState extends State<ComplexData> {
                 future: getUserApi(),
                 builder: (context,snapshot){
                   if(snapshot.connectionState==ConnectionState.waiting){
-                    return Text("loading");
+                    return Text("Loading");
                   }else {
-                    return Text(data[0]["name"].toString());
+                     return ListView.builder(
+                       itemCount: data.length,
+                         itemBuilder:(context,index){
+                       return Card(
+                         child: Column(
+                           children: [
+                             ReuseableRow(title: "id:-",value: data[index]["id"].toString(),),
+                             ReuseableRow(title: "name:-",value: data[index]["name"].toString(),),
+                             ReuseableRow(title: "username:-",value: data[index]["username"].toString(),),
+                             ReuseableRow(title: "email:-",value: data[index]["email"].toString(),),
+                             ReuseableRow(title: "addree:-",value: data[index]["address"].toString(),),
+                             ReuseableRow(title: "street:-",value: data[index]["address"]["street"].toString(),),
+                             ReuseableRow(title: "phone:-",value: data[index]["phone"].toString(),),
+                             ReuseableRow(title: "website:-",value: data[index]["website"].toString(),),
+                           ],
+                         )
+                       );
+                     });
+                    // Text(data[0]["username"].toString());
                   }
                 },
               ) )
@@ -49,3 +67,24 @@ class _ComplexDataState extends State<ComplexData> {
     );
   }
 }
+class ReuseableRow extends StatelessWidget {
+  String title, value;
+   ReuseableRow({Key? key,required this.title, required this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+      child:Row(
+       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(title),
+          Text(value),
+
+        ]
+
+      )
+    );
+  }
+}
+
