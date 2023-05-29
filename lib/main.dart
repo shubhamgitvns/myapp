418521a7dd68d748f4cafaec0c6f4cb6c9b1a945
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-29 22:34:06 +0530

    simple add and subtract function using text field

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,81 +14,110 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("App"),
+          backgroundColor: Colors.teal,
+          title: const Text("App"),
           centerTitle: true,
         ),
-        body: Column(
+        body: const Adder(),
+      ),
+    );
+  }
+}
+
+class Adder extends StatefulWidget {
+  const Adder({Key? key}) : super(key: key);
+
+  @override
+  State<Adder> createState() => _AdderState();
+}
+
+class _AdderState extends State<Adder> {
+  String a = "", b = "";
+  String result = "";
+  void add() {
+    try {
+      result = (int.parse(a) + int.parse(b)).toString();
+    } catch (e) {
+      result = "";
+    }
+    setState(() {});
+  }
+
+  void sub(){
+    try{
+      result=(int.parse(a) - int.parse(b)).toString();
+    }catch(e){
+      result="";
+    }
+    setState(() {});
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            SizedBox(
+              //size box like a container
+              width: 200,
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox( //size box like a container
-                  width: 200,
+              child: TextField(
+                  keyboardType: TextInputType
+                      .number, // input type work in mobile keyboard
+                  onChanged: (value) {
+                    print(value);
+                    a = value;
+                   // calculate();
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'A', //label text work like a placeholder in css
+                  )),
+            ),
+            SizedBox(
+              //size box like a container
+              width: 200,
 
-                  child: TextField(
-                    keyboardType: TextInputType.number,// input type work in mobile keyboard
-                      onChanged: (value) { //print entered value in console
-                        print(value);
-                      },
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Number',//label text work like a placeholder in css
-                      )),
-                )
-              ],
+              child: TextField(
+                  keyboardType: TextInputType
+                      .number, // input type work in mobile keyboard
+                  onChanged: (value) {
+                    //print entered value in console
+                    print(value);
+                    b = value;
+                   // calculate();
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'B', //label text work like a placeholder in css
+                  )),
+            ),
+            Text(result),
+            ElevatedButton(
+                onPressed:(){
+                  add();
+                  print(result);
+
+            },
+                child: Text("Add")
             ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 50, 90, 70),//this is padding style (left)(top)(right)(bottom)
-                  child: SizedBox(
-                    width: 200,
+            ElevatedButton(
+                onPressed:(){
+                  sub();
+                  print( result);
 
-                    child: TextField(
-
-                      obscureText: true,// this is hide the input value
-                        keyboardType: TextInputType.name,
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Name',
-                        )),
-                  ),
-                )
-              ],
+                },
+                child: Text("Sub")
             ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 200,
-
-                    child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'email',
-                        )),
-                  ),
-                )
-              ],
-            )
           ],
         ),
-      ),
+      ],
     );
   }
 }
