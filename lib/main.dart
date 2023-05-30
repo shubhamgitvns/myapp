fc9a6dd801ba8c8e3bc8e41cc0a5cb710098e3d3
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-30 11:35:07 +0530

    Third type of list view

--- lib/main.dart
+++ lib/main.dart
@@ -32,24 +32,24 @@ class Adder extends StatefulWidget {
 
 class _AdderState extends State<Adder> {
 
-
-  final List<String> entries = <String>['A', 'B', 'C','d'];//create a list show list name
-  final List<int> colorCodes = <int>[600, 500, 100,50];//create the list show color code
-
-
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: entries.length, //item length
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          height: 50,
-          color: Colors.amber[colorCodes[index]],
-          child: Center(child: Text('Entry ${entries[index]}')),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverPadding(
+          padding: const EdgeInsets.all(20.0),
+          sliver: SliverList(
+            delegate: SliverChildListDelegate(
+              <Widget>[
+                const Text("I'm dedicating every day to you"),
+                const Text('Domestic life was never quite my style'),
+                const Text('When you smile, you knock me out, I fall apart'),
+                const Text('And I thought I was so smart'),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
