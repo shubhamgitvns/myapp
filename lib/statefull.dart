088627c1be06371f03aa4a6835d21ed182e38630
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-30 12:46:43 +0530

    Silver grid and Silver fixed extent list

--- lib/statefull.dart
+++ lib/statefull.dart
@@ -8,57 +8,58 @@ class CustomScrollViewExample extends StatefulWidget {
 }
 
 class _CustomScrollViewExampleState extends State<CustomScrollViewExample> {
-  List<int> top = <int>[];
-  List<int> bottom = <int>[0];
 
   @override
   Widget build(BuildContext context) {
     const Key centerKey = ValueKey<String>('bottom-sliver-list');
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Press on the plus to add items above and below'),
-        leading: IconButton( // Icon (+)
-          icon: const Icon(Icons.add),//Add the new list
-          onPressed: () {
-            setState(() {
-              top.add(top.length - 1);
-              bottom.add(bottom.length);
-            });
-          },
-        ),
+        title: const Text('Custom scroll view class'),
+
       ),
       body: CustomScrollView(
-        center: centerKey,
         slivers: <Widget>[
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.blue[200 + top[index] % 4 * 100],
-                  height: 100 + top[index] % 4 * 20.0,
-                  child: Text('Item: ${top[index]}'),
-                );
-              },
-              childCount: top.length,
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Demo'),
             ),
           ),
-          SliverList(
-            key: centerKey,
+
+          SliverGrid( //silver grid its your choice create multiple grid
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
+            ),
             delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index) {
                 return Container(
                   alignment: Alignment.center,
-                  color: Colors.blue[200 + bottom[index] % 4 * 100],
-                  height: 100 + bottom[index] % 4 * 20.0,
-                  child: Text('Item: ${bottom[index]}'),
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('Grid Item $index'),
                 );
               },
-              childCount: bottom.length,
+              childCount: 40,//grid count
+            ),
+          ),
+
+          SliverFixedExtentList(  //Silver extent list
+            itemExtent: 50.0, //item extent
+            delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text('List Item $index'),
+                );
+              },
             ),
           ),
         ],
-      ),
+      )
     );
   }
 }
