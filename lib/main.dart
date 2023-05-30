527ea13b4d9feb0fc91a7e77589fb53e6cfbe147
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-30 12:15:16 +0530

    click the icon and create the new list

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/stateless.dart';
 
-void main() {
-  runApp(MyApp());
-}
+/// Flutter code sample for [CustomScrollView].
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+void main() => runApp(const CustomScrollViewExampleApp());
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: const Text("App"),
-          centerTitle: true,
-        ),
-        body: const Adder(),
-      ),
-    );
-  }
-}
-
-class Adder extends StatefulWidget {
-  const Adder({Key? key}) : super(key: key);
-
-  @override
-  State<Adder> createState() => _AdderState();
-}
-
-class _AdderState extends State<Adder> {
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverPadding(
-          padding: const EdgeInsets.all(20.0),
-          sliver: SliverList(
-            delegate: SliverChildListDelegate(
-              <Widget>[
-                const Text("I'm dedicating every day to you"),
-                const Text('Domestic life was never quite my style'),
-                const Text('When you smile, you knock me out, I fall apart'),
-                const Text('And I thought I was so smart'),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/stateless.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/statefull.dart';
+
+
+class CustomScrollViewExampleApp extends StatelessWidget {
+  const CustomScrollViewExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CustomScrollViewExample(),
+    );
+  }
+}
